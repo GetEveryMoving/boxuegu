401aeda536436bf5c62c628ac0793c2f3c4a9172
jet <506323170@qq.com>
2017-02-26 08:41:57 +0800

    布局完成

---
+++ js/main.js
@@ -0,0 +1,34 @@
+/**
+ * Created by 熊旭文 on 2017/2/25.
+ */
+requirejs.config({
+    baseUrl:'/',
+    paths:{
+        //第三方的路径配置
+        jquery:'lib/jquery/jquery.min',
+        bootstrap:'lib/bootstrap/js/bootstrap',
+        //自己配置的路径
+        userList:'js/user/list',
+        userProfile:'js/user/profile'
+    },
+
+    shim:{
+        bootstrap:{
+            deps:["jquery"]
+        }
+    }
+});
+require(['jquery','bootstrap']);
+
+(function(window){
+    var pathname=window.location.pathname;
+    switch(pathname){
+        case "/html/user/list.html":
+            require(['userList']);
+            break;
+        case "/html/user/profile.html":
+            require(['userProfile']);
+            break;
+    }
+})(window);
+
